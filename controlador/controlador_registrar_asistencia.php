1c1bd71de41d21a1d824ffa2c112b99d57407d29
juanAriel <arielvia2013@gmail.com>
2022-11-28 10:16:27 -0400

    registrando la salida del usuario con el ultimo registro encontrado

--- controlador/controlador_registrar_asistencia.php
+++ controlador/controlador_registrar_asistencia.php
@@ -7,31 +7,30 @@ if (!empty($_POST["btnentrada"])) {
         if ($consulta->fetch_object()->total > 0) {
             $fecha = date("y-m-d h:i:s");
             $id_empleado = $id->fetch_object()->id_empleado;
-            $sql=$conexion->query(" insert into asistencia(id_empleado,entrada)values($id_empleado,'$fecha')");
-            if ($sql==true) { ?>
+            $sql = $conexion->query(" insert into asistencia(id_empleado,entrada)values($id_empleado,'$fecha')");
+            if ($sql == true) { ?>
                 <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "Correcto",
-                        type: "success",
-                        text: "hola bienvenido",
-                        styling: "bootstrap3"
+                    $(function notificacion() {
+                        new PNotify({
+                            title: "Correcto",
+                            type: "success",
+                            text: "hola bienvenido",
+                            styling: "bootstrap3"
+                        })
                     })
-                })
-            </script>
+                </script>
             <?php } else { ?>
                 <script>
-                $(function notificacion() {
-                    new PNotify({
-                        title: "Incorrecto",
-                        type: "error",
-                        text: "Error al registrar su entrada",
-                        styling: "bootstrap3"
+                    $(function notificacion() {
+                        new PNotify({
+                            title: "Incorrecto",
+                            type: "error",
+                            text: "Error al registrar su entrada",
+                            styling: "bootstrap3"
+                        })
                     })
-                })
-            </script>
+                </script>
             <?php }
-            
         } else { ?>
             <script>
                 $(function notificacion() {
@@ -55,11 +54,79 @@ if (!empty($_POST["btnentrada"])) {
                 })
             })
         </script>
-<?php }?>
-<script>
-    setTimeout(() => {
-        window.history.replaceState(null,null,window.location.pathname);
-    }, 0);
-</script>
+    <?php } ?>
+    <script>
+        setTimeout(() => {
+            window.history.replaceState(null, null, window.location.pathname);
+        }, 0);
+    </script>
+<?php }
+?>
+
+
+<?php
+if (!empty($_POST["btnsalida"])) {
+    if (!empty($_POST["txtci"])) {
+        $dni = $_POST["txtci"];
+        $consulta = $conexion->query(" select count(*) as 'total' from empleado where dni='$dni' ");
+        $id = $conexion->query("select id_empleado from empleado where dni='$dni' ");
+        if ($consulta->fetch_object()->total > 0) {
+            $fecha = date("y-m-d h:i:s");
+            $id_empleado = $id->fetch_object()->id_empleado;
+            $busqueda = $conexion->query("select id_asistencia from asistencia where id_empleado=$id_empleado order by id_asistencia desc limit 1");
+            $id_asistencia = $busqueda->fetch_object()->id_asistencia;
+            $sql = $conexion->query(" update asistencia set salida='$fecha' where id_asistencia=$id_asistencia ");
+            if ($sql == true) { ?>
+                <script>
+                    $(function notificacion() {
+                        new PNotify({
+                            title: "Correcto",
+                            type: "success",
+                            text: "Adios hasta luego",
+                            styling: "bootstrap3"
+                        })
+                    })
+                </script>
+            <?php } else { ?>
+                <script>
+                    $(function notificacion() {
+                        new PNotify({
+                            title: "Incorrecto",
+                            type: "error",
+                            text: "Error al registrar su salida",
+                            styling: "bootstrap3"
+                        })
+                    })
+                </script>
+            <?php }
+        } else { ?>
+            <script>
+                $(function notificacion() {
+                    new PNotify({
+                        title: "Incorrecto",
+                        type: "error",
+                        text: "el DNI no existe",
+                        styling: "bootstrap3"
+                    })
+                })
+            </script>
+        <?php  }
+    } else { ?>
+        <script>
+            $(function notificacion() {
+                new PNotify({
+                    title: "Incorrecto",
+                    type: "error",
+                    text: "el campo DNI esta vacio",
+                    styling: "bootstrap3"
+                })
+            })
+        </script>
+    <?php } ?>
+    <script>
+        setTimeout(() => {
+            window.history.replaceState(null, null, window.location.pathname);
+        }, 0);
+    </script>
 <?php }
 ?>
